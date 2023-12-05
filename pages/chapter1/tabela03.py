0ba94aadc0feca54b28e8d93cfaaacf9132b66cd
Rivaldo da Silva Junior <rivaldoj@see.pb>
2023-12-05 16:03:00 -0300

    comitando

--- pages/chapter1/tabela03.py
+++ pages/chapter1/tabela03.py
@@ -5,38 +5,67 @@ import pandas as pd
 import numpy as np
 import plotly.graph_objects as go
 
-register_page(__name__, icon="fa:table", name='Escolas Técnicas')
+register_page(__name__, icon="fa:table", name='03 - Escolas Técnicas')
 #Leitura do dataset
 dados = pd.read_csv('./assets/data/dados_completos_644_escolas.csv', sep=',', low_memory=False)
 dados.drop(columns='Unnamed: 0', inplace=True)
+dados_16_anos = pd.read_csv('./assets/data/dados_completos_644_escolas.csv', sep=',', low_memory=False)
+dados_16_anos.drop(columns='Unnamed: 0', inplace=True)
 
-#Seleção dos dados para criar tabelas de conectividade
-dados_conectividade = dados[[
-    'NU_ANO_CENSO', 'NO_ENTIDADE', 'CO_ORGAO_REGIONAL',
-    'TP_LOCALIZACAO', 'IN_LABORATORIO_INFORMATICA', 
-    'IN_BIBLIOTECA', 'IN_BIBLIOTECA_SALA_LEITURA', 
-    'IN_INTERNET', 'IN_INTERNET_ALUNOS', 
-    'IN_INTERNET_ADMINISTRATIVO', 'IN_INTERNET_APRENDIZAGEM',
-    'IN_BANDA_LARGA','TP_REDE_LOCAL'
-]].copy()
+escolas_profissionalizantes_16_anos = dados_16_anos.groupby('NU_ANO_CENSO')[['IN_PROF']].sum()
+escolas_profissionalizantes_16_anos.rename(columns={'IN_PROF':'Número de Escolas Técnicas'}, inplace=True)
+escolas_profissionalizantes_16_anos.rename_axis('Ano do Censo', inplace=True)
 
-#Renomeando os campos para utilizar
-dados_conectividade.rename(columns={
-    'NU_ANO_CENSO':'Ano do Censo', 'NO_ENTIDADE':'Nome da Escola','CO_ORGAO_REGIONAL':'Gerencia Regional' ,
-    'TP_LOCALIZACAO':'Tipo de Area', 'IN_LABORATORIO_INFORMATICA':'Laboratorio de informática', 
-    'IN_BIBLIOTECA': 'Biblioteca', 'IN_BIBLIOTECA_SALA_LEITURA':'Biblioteca ou Sala de Leitura', 
-    'IN_INTERNET':'Acesso a Internet', 'IN_INTERNET_ALUNOS':'Internet para Alunos', 
-    'IN_INTERNET_ADMINISTRATIVO':'Internet para Administração', 'IN_BANDA_LARGA':'Internet Banda Larga',
-    'TP_REDE_LOCAL': 'Tipo de Rede Local', 'IN_INTERNET_APRENDIZAGEM':'Internet para Aprendizagem'
-    },
-    inplace=True
+df_escolas_profissionalizantes_16_anos = pd.DataFrame({
+    'Ano do Censo': escolas_profissionalizantes_16_anos.index,
+    'Número de Escolas Profissionalizantes': escolas_profissionalizantes_16_anos['Número de Escolas Técnicas'].array
+})
+df_escolas_profissionalizantes_16_anos.rename_axis('', inplace=True)
+df_escolas_profissionalizantes_16_anos.reset_index(drop=True, inplace=True)
+
+years = [ 2007, 2008, 2009, 2010, 2011, 2012, 2013, 2014, 
+        2015, 2016, 2017, 2018, 2019, 2020, 2021, 2022]
+########GRAFICO EVOLUÇÃO DOS ECITS##################
+fig = go.Figure()
+fig.add_trace(go.Line(x=years,
+                y=dados_16_anos['IN_PROF'],
+                name='Número de escolas',
+                marker_color='rgb(55, 83, 109)', 
+                text=dados_16_anos['IN_PROF']
+                ))
+fig.update_xaxes(dtick=[ 2007, 2008, 2009, 2010, 2011, 2012, 2013, 2014, 
+        2015, 2016, 2017, 2018, 2019, 2020, 2021, 2022])
+fig.update_layout(
+    title='Evolução das Escolas Técnicas Estaduais de 2007 a 2022',
+    title_font_size=22,
+    xaxis_tickfont_size=14,
+    yaxis=dict(
+        title='Número de Escolas',
+        titlefont_size=18,
+        tickfont_size=12,
+    ),
+    xaxis=dict(
+        title='Ano do Censo',
+        titlefont_size=18,
+        tickfont_size=12,
+    ),
+    legend=dict(
+        x=0.8,
+        y=1.0,
+        bgcolor='rgba(255, 255, 255, 0)',
+        bordercolor='rgba(255, 255, 255, 0)'
+    ),
+    barmode='group',
+    bargap=0.15, # gap between bars of adjacent location coordinates.
+    bargroupgap=0.1, # gap between bars of the same location coordinate.
+    autosize =True,
+    width = 1400,
+    height = 600
 )
-
 
 layout = html.Div(
     children=[
-        html.Div('Tabela 03')
-            
-        
+        html.Div(dcc.Graph(figure=fig)),
+        html.Br(),
     ]
 )
--- pages/chapter1/tabelas01.py
+++ pages/chapter1/tabelas01.py
@@ -5,7 +5,7 @@ import pandas as pd
 import numpy as np
 import plotly.graph_objects as go
 
-register_page(__name__, icon="fa:table", name='Internet - Escolas e Apredizagem')
+register_page(__name__, icon="fa:table", name='01 - Internet - Escolas e Apredizagem')
 #Leitura do dataset
 dados = pd.read_csv('./assets/data/dados_completos_644_escolas.csv', sep=',', low_memory=False)
 dados.drop(columns='Unnamed: 0', inplace=True)
